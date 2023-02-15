0c8967a70ab9c46c248ca8c50720e1793f655617
Marco Bonici <bonici.marco@gmail.com>
2023-02-15 01:32:42 +0100

    First implementation of backward rule

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -4,3 +4,13 @@ function ChainRulesCore.frule((_, Δalm, _), ::typeof(alm2map), alm, nside::Inte
     Δy = alm2map(Δalm, nside)
     return y, Δy
 end
+
+function ChainRulesCore.rrule(::typeof(alm2map), x, nside::Integer)
+    y = alm2map(x, nside)
+    project_x = ChainRulesCore.ProjectTo(x)
+    function alm2map_pullback(ȳ)
+        x̄ = project_x(adjoint_alm2map(ChainRulesCore.unthunk(ȳ), nside))
+        return ChainRulesCore.NoTangent(), x̄, ChainRulesCore.NoTangent()
+    end
+    return y, alm2map_pullback
+end
