ca335fdfdd65826cd4852bd80022968b817ca97f
Marco Bonici <bonici.marco@gmail.com>
2023-02-15 01:29:16 +0100

    Added nonmutating versions of adjoint_maptoalm

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,4 +1,5 @@
 function ChainRulesCore.frule((_, Δalm, _), ::typeof(alm2map), alm, nside::Integer)
+    #TODO added, intentionally, no signature to alm
     y = alm2map(alm, nside)
     Δy = alm2map(Δalm, nside)
     return y, Δy
